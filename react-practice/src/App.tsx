df9f9f7c35e866c1ba1f9242571498983ab7ea14
Corey <coreyjacob2002@gmail.com>
2025-08-25 13:48:58 -0400

    fixed imports

--- react-practice/src/App.tsx
+++ react-practice/src/App.tsx
@@ -4,7 +4,7 @@ import Sidebar from './components/Sidebar/Sidebar';
 import MainContent from "./components/MainContent";
 import { BoxesProvider } from "./components/Boxes/Box/context/BoxesProvider";
 import Header from "./components/Header";
-import Footer from "./components/Header";
+import Footer from "./components/Footer";
 
 
 function App() {
